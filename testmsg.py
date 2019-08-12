2e50a135cad5c262923b8408027774a88a42b866
Christian Tacke <8560110+ChristianTacke@users.noreply.github.com>
2019-08-12 15:14:11 +0200

    Small test tool for messages.
    This small tool calls the argument parser with nearly all
    possible/needed arguments and shows the resulting message.
    This helped a lot in improving messages.

---
+++ testmsg.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from pprint import pprint
+from xmppnotify import build_argparser, build_message
+
+
+commonargs = \
+    ["--hostname", "HOSTNAME",
+     "--hostdisplayname", "HOSTDISPLAYNAME",
+     "-o", "outputline1\noutputline2",
+     "--hostaddress", "127.0.0.1",
+     "--hostaddress6", "::1",
+     "--icingaweb2url", "ICINGAWEB2URL",
+     "--state", "STATE", "--longdatetime", "2019-12-31 23:59:59"]
+
+if __name__ == '__main__':
+    parser = build_argparser()
+
+    args = parser.parse_args(commonargs +
+                             ["--notificationtype", "PROBLEM",
+                              "-e", "servicename",
+                              "--servicedisplayname", "servicedisplayname"])
+    pprint(build_message(args))
+
+    args = parser.parse_args(commonargs +
+                             ["--notificationtype", "RECOVERY",
+                              "--notificationauthorname",
+                              "NOTIFICATIONAUTHORNAME",
+                              "--notificationcomment",
+                              "NOTIFICATIONCOMMENT\nMORE COMMENTS"])
+    pprint(build_message(args))
